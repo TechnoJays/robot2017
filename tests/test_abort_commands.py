3f35f407e01edb86040dfebeca21bca894800479
Aaron DeGrow <adegrow@vectorform.com>
2017-02-18 11:34:37 -0500

    Fix broken tests due to refactoring

--- tests/test_abort_commands.py
+++ tests/test_abort_commands.py
@@ -1,13 +1,13 @@
 import pytest
-from subsystems.gear_release import GearRelease
+from subsystems.gear_feeder import GearFeeder
 from subsystems.winch import Winch
 from subsystems.drivetrain import Drivetrain
 from commands.abort_commands import Abort
 
 
 @pytest.fixture(scope="function")
-def gear_release_default(robot):
-    return GearRelease(robot, None, '../tests/test_configs/gear_release_default.ini')
+def gear_feeder_default(robot):
+    return GearFeeder(robot, None, '../tests/test_configs/gear_feeder_default.ini')
 
 
 @pytest.fixture(scope="function")
@@ -21,8 +21,8 @@ def drivetrain_default(robot):
 
 
 @pytest.fixture(scope="function")
-def command_default(robot, gear_release_default, winch_default, drivetrain_default):
-    robot.gear_release = gear_release_default
+def command_default(robot, gear_feeder_default, winch_default, drivetrain_default):
+    robot.gear_feeder = gear_feeder_default
     robot.winch = winch_default
     robot.drivetrain = drivetrain_default
     return Abort(robot, None, 15)
@@ -31,21 +31,21 @@ def command_default(robot, gear_release_default, winch_default, drivetrain_defau
 def test_init_default(command_default):
     assert command_default is not None
     assert command_default.robot is not None
-    assert command_default.robot.gear_release is not None
+    assert command_default.robot.gear_feeder is not None
     assert command_default.robot.winch is not None
     assert command_default.robot.drivetrain is not None
     assert command_default.name == "Abort"
     assert command_default.timeout == 15
 
 
-def test_init_full(robot, gear_release_default, winch_default, drivetrain_default):
-    robot.gear_release = gear_release_default
+def test_init_full(robot, gear_feeder_default, winch_default, drivetrain_default):
+    robot.gear_feeder = gear_feeder_default
     robot.winch = winch_default
     robot.drivetrain = drivetrain_default
     abrt = Abort(robot, "CustomAbort", 5)
     assert abrt is not None
     assert abrt.robot is not None
-    assert abrt.robot.gear_release is not None
+    assert abrt.robot.gear_feeder is not None
     assert abrt.robot.winch is not None
     assert abrt.robot.drivetrain is not None
     assert abrt.name == "CustomAbort"
@@ -56,8 +56,8 @@ def test_initialize(command_default):
     assert command_default._ran_once is False
 
 
-def test_execute(robot, hal_data, gear_release_default, winch_default, drivetrain_default):
-    robot.gear_release = gear_release_default
+def test_execute(robot, hal_data, gear_feeder_default, winch_default, drivetrain_default):
+    robot.gear_feeder = gear_feeder_default
     robot.winch = winch_default
     robot.drivetrain = drivetrain_default
     abrt = Abort(robot, None, None)
