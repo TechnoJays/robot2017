072eb81ac8efb5b4df0b61bd3b90180ba4e6a71c
Aaron DeGrow <adegrow@vectorform.com>
2017-02-18 11:46:01 -0500

    Change auto mode to use drive time

--- src/commands/autonomous_default.py
+++ src/commands/autonomous_default.py
@@ -1,6 +1,7 @@
 import configparser
 from wpilib.command import CommandGroup
 from commands.drive_encoder_counts import DriveEncoderCounts
+from commands.drive_time import DriveTime
 from commands.abort_commands import Abort
 
 
@@ -9,6 +10,7 @@ class AutonomousDefault(CommandGroup):
     _approach_speed_key = "APPROACH_SPEED"
     _approach_encoder_counts_key = "APPROACH_ENCODER_COUNTS"
     _approach_encoder_threshold_key = "APPROACH_ENCODER_THRESHOLD"
+    _approach_time_key = "APPROACH_TIME"
 
     _robot = None
     _config = None
@@ -18,6 +20,7 @@ class AutonomousDefault(CommandGroup):
     _approach_speed = None
     _approach_encoder_counts = None
     _approach_encoder_threshold = None
+    _approach_time = None
 
     _starting_position = None
 
@@ -42,11 +45,15 @@ class AutonomousDefault(CommandGroup):
                                                             AutonomousDefault._approach_encoder_counts_key)
         self._approach_encoder_threshold = self._config.getint(AutonomousDefault._approach_section,
                                                                AutonomousDefault._approach_encoder_threshold_key)
+        self._approach_time = self._config.getint(AutonomousDefault._approach_section,
+                                                  AutonomousDefault._approach_time_key)
 
     def _add_approach_commands(self):
-        self._approach_commands.addSequential(DriveEncoderCounts(self._robot, self._approach_encoder_counts_key,
-                                                                 self._approach_speed_key,
-                                                                 self._approach_encoder_threshold_key),
+        # self._approach_commands.addSequential(DriveEncoderCounts(self._robot, self._approach_encoder_counts_key,
+        #                                                          self._approach_speed_key,
+        #                                                          self._approach_encoder_threshold_key),
+        #                                       self._default_timeout)
+        self._approach_commands.addSequential(DriveTime(self._robot, self._approach_time, self._approach_speed),
                                               self._default_timeout)
         self.addSequential(self._approach_commands)
 
